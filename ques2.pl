44cea2a9b11fa155f198be397cecd66701738cf8
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-02-08 10:45:56 -0500

    ques2: cleanup and formatting

--- ques2.pl
+++ ques2.pl
@@ -1,9 +1,15 @@
-% [a, 100, b, [200, c, 100], c, 300, d]
-
 sum-up-numbers-general([], 0).
-sum-up-numbers-general(L, N) :-  L = [A|B], \+number(A), sum-up-numbers-general(B, N).
-sum-up-numbers-general(L, N) :-  L = [A|B], number(A), sum-up-numbers-general(B, N1), N is A + N1.
-sum-up-numbers-general(L, N) :- L = [A|B], is_list(A), 
+sum-up-numbers-general(L, N) :-  
+								L = [A|B], 
+								\+number(A), 
+								sum-up-numbers-general(B, N).
+sum-up-numbers-general(L, N) :-  
+								L = [A|B], 
+								number(A), 
+								sum-up-numbers-general(B, N1), N is A + N1.
+sum-up-numbers-general(L, N) :- 
+								L = [A|B], 
+								is_list(A), 
 								sum-up-numbers-general(A, N1), 
 								sum-up-numbers-general(B, N2),
 								N is N1 + N2.
