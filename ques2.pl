3e2a8b2759754bb3c3dd1f52421f05526c9d19f7
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-02-08 10:48:41 -0500

    Ques2: added comments

--- ques2.pl
+++ ques2.pl
@@ -1,12 +1,19 @@
-sum-up-numbers-general([], 0).
+% case for empty list 
+sum-up-numbers-general([], 0).   
+
+% case for when the atom is not a number
 sum-up-numbers-general(L, N) :-  
 								L = [A|B], 
 								\+number(A), 
 								sum-up-numbers-general(B, N).
+
+% case for when atom is a number
 sum-up-numbers-general(L, N) :-  
 								L = [A|B], 
 								number(A), 
 								sum-up-numbers-general(B, N1), N is A + N1.
+
+% case for when first element is list
 sum-up-numbers-general(L, N) :- 
 								L = [A|B], 
 								is_list(A), 
