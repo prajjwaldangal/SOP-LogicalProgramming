d38975ebf26c7428f4078365d0d87a941ce48efa
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-02-08 04:14:04 -0500

    Ques2: some modifications

--- ques2.pl
+++ ques2.pl
@@ -1,6 +1,6 @@
-% [a, 100, b, [200], c, 300, d]
+% [a, 100, b, [200, c, 100], c, 300, d]
 
-sum-up-numbers-general(L, N) :-  L = [A|B], 
+sum-up-numbers-general(L, N) :-  L = [A|B], \+number(A), sum-up-numbers-general(B, N).
+sum-up-numbers-general(L, N) :-  L = [A|B], number(A), sum-up-numbers-general(B, N1), N is A + N1.
 sum-up-numbers-general([], 0).
-sum-up-numbers-general(L, N) :-
-						d
+sum-up-numbers-general(L, N) :-  L = [A|B], is_List(A), sum-up-numbers-general(A, N1).
