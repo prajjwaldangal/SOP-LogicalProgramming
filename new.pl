12221f19d6dac529b11ce4512fa1e0d646908c11
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-02-08 03:31:07 -0500

    explored replacing negation number method by charlist -> ques.1

--- new.pl
+++ new.pl
@@ -3,10 +3,19 @@ sum-up-numbers-simple(L, N) :-
 								L = [A|B],
 								\+number(A),
 								sum-up-numbers-simple(B, N).
-
-
 sum-up-numbers-simple([], 0).
 sum-up-numbers-simple(L, N)  :-   
 								L = [A|B],
 								number(A), 
 								sum-up-numbers-simple(B, N1), N is A + N1.
+
+
+% L1 -> [100, 300, 200],      L2 -> [300, 100, 200, a]
+
+min([],Y)  :-  Y.
+min(X,Y,X) :-  X < Y.
+min(X,Y,Y) :-  Y < X.
+
+min-above-min(L1, L2, N) :- 
+						L2 = [A|B],
+						number(A).
